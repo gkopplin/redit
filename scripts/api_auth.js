05799b2d70d213d7f062a8277ad8cfe6080fb534
gkopplin <grant.kopplin@gmail.com>
2019-09-25 16:05:10 -0400

    Remove duplicate code from merge

--- scripts/api_auth.js
+++ scripts/api_auth.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 export const signup = (username, email, password) => {
-=======
-export const signup = (email, username, password) => {
->>>>>>> 9fe00a40f77eeea84362ef4e4e73bea77ed79545
     fetch('http://thesi.generalassemb.ly:8080/signup', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            email,
             username,
             email,
             password
@@ -18,6 +13,7 @@ export const signup = (email, username, password) => {
     .then(response => response.json())
     .then(response => {
         console.log(response);
+        localStorage.setItem('auth_key', response.token);
     })
     .catch(err => console.log(err));
 };
--- scripts/auth.js
+++ scripts/auth.js
@@ -7,7 +7,6 @@ export default () => {
     const form = document.createElement('form');
     const email = document.createElement('input');
     const username = document.createElement('input');
-    const email = document.createElement('input');
     const password = document.createElement('input');
     const close = document.createElement('a');
     const submit = document.createElement('button');
@@ -19,26 +18,16 @@ export default () => {
     close.className = 'close';
     email.className = 'email';
     username.className = 'username';
-    email.className = 'email';
     password.className = 'password';
     submit.className = 'submit';
 
     // Prep elements
     email.setAttribute('placeholder','Enter a valid email address');
     username.setAttribute('placeholder', 'Enter a username');
-    email.setAttribute('placeholder', 'Enter your email');
     password.setAttribute('placeholder', 'Enter a password');
     close.innerHTML = '&#10060';
     close.onclick = () => {
         auth.style.visibility = "hidden";
-<<<<<<< HEAD
-
-    };
-    form.onsubmit = e => {
-        e.preventDefault();
-        signup(username.value, email.value, password.value);
-    };
-=======
         console.log(username.value);
     };
 
@@ -48,12 +37,10 @@ export default () => {
       signup(email.value, username.value, password.value);
     };
 
->>>>>>> 9fe00a40f77eeea84362ef4e4e73bea77ed79545
     submit.setAttribute('type', 'submit');
     submit.innerText = 'Submit';
 
     // Append elements
-    form.append(email);
     form.append(username);
     form.append(email);
     form.append(password);
