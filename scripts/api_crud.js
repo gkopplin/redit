53224c79b634a89042f440d16a74f477ab3a825f
Christopher Cook <cgcook81@gmail.com>
2019-09-26 14:47:14 -0400

    delete post added

--- scripts/api_crud.js
+++ scripts/api_crud.js
@@ -8,15 +8,31 @@ export const createPost = (title, description) => {
             'Authorization': `Bearer ${localStorage.getItem('auth_key')}`
         },
         body: JSON.stringify({
-            title, 
+            title,
             description
         }),
     })
         .then(response => response.json())
         .then(response => {
             console.log(response);
-            const post = createPostItem(response.title, response.description);
+            const post = createPostItem(response.title, response.description, response.id);
             document.querySelector('.homepage').append(post);
         })
         .catch(err => console.log(err));
+};
+
+//display error in thml vs console.log
+export const deletePost = (postId) => {
+  fetch(`http://thesi.generalassemb.ly:8080/post/${postId}`, {
+      method: 'DELETE',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('auth_key')}`
+      }
+  })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => console.log(err));
 };
--- scripts/post.js
+++ scripts/post.js
@@ -1,22 +1,33 @@
-export default (title, description) => {
+import { deletePost } from './api_crud.js';
+
+export default (title, description, postId) => {
 
   //create elements
   const post = document.createElement('div');
   const postTitle = document.createElement('h1');
   const body = document.createElement('p');
+  const delButton = document.createElement('button');
 
   //add classnames
     post.className = 'post';
     postTitle.className = 'title';
     body.className = 'body';
+    delButton.className = 'delete';
 
   //elements
     postTitle.innerHTML = title;
     body.innerHTML = description;
+    delButton.innerHTML = 'Delete';
+
+    delButton.onclick = () => {
+      deletePost(postId);
+    };
 
   //append
     post.append(postTitle);
     post.append(body);
+    post.append(delButton);
+
 
     return post;
 };
