aca788d6161b623ab405adca296f3764120439f9
gkopplin <grant.kopplin@gmail.com>
2019-09-26 15:09:26 -0400

    Add initial code for displaying one post

--- scripts/post.js
+++ scripts/post.js
@@ -7,26 +7,42 @@ export default (title, description, postId) => {
   const postTitle = document.createElement('h1');
   const body = document.createElement('p');
   const delButton = document.createElement('button');
+  const addComment = document.createElement('button');
 
   //add classnames
     post.className = 'post';
     postTitle.className = 'title';
     body.className = 'body';
     delButton.className = 'delete';
+    addComment.classList.add('add-comment', 'logged-in');
 
   //elements
     postTitle.innerHTML = title;
     body.innerHTML = description;
     delButton.innerHTML = 'Delete';
+    addComment.innerHTML = 'Add comment';
 
     delButton.onclick = () => {
       deletePost(postId);
     };
 
+    // test this
+    post.onclick = () => {
+      const allPosts = document.querySelectorAll('.post');
+
+      for (let postItem of allPosts) {
+        postItem.style.display = 'none';
+      }
+      
+      post.style.display = 'inline';
+    };
+
+
   //append
     post.append(postTitle);
     post.append(body);
     post.append(delButton);
+    post.append(addComment);
 
 
     return post;
