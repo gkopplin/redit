8dde319e1b41d9c0132c1f8eb21bdfa8874af3c9
Christopher Cook <cgcook81@gmail.com>
2019-09-28 16:21:23 -0400

    fix local files

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -3,9 +3,7 @@ import {fetchPosts, fetchPostbyId} from './api_crud.js';
 
 export const createHomepage = () => {
   let homepage = document.createElement('div');
-  const scrollWindow = document.createElement('div');
   homepage.className = 'homepage';
-  scrollWindow.className = 'scroll-window';     //me
 
   homepage.append(createHeader());
   if (window.location.hash.length === 0) {
@@ -14,39 +12,5 @@ export const createHomepage = () => {
     fetchPostbyId(window.location.hash);
   }
   document.body.append(homepage);
-  homepage.append(scrollWindow);         //me
-
-  //infinite scroll - create intersection observer
-// const createObserver = () => {
-//   let observer;
-//   // let target = document.querySelector('.scroll-window').lastChild;
-//     let scrolling = {
-//       root: null,
-//       // root: document.querySelector('.header'),
-//       rootMargin: '0px',
-//       threshold: 1.0
-//     };
-//     // let target = document.querySelector('.scroll-window').lastChild;
-//   observer = new IntersectionObserver(handleIntersect, scrolling);
-//   observer.observe(target);
-// };
-// function handleIntersect(entries, observer) {
-//   entries.forEach(entry => {
-//     // entry.target;
-//     console.log('handleIntersect firing');
-//   });
-// };
-// let target;
-
-//the example setup I found above isn't triggering appropriately
-window.addEventListener ('click', () => {
-  let target = document.querySelector('.scroll-window');
-  // createObserver();
-  fetchPosts();
-  console.log('EventListener triggered');
-}, false);
-
-
-
 
 };
